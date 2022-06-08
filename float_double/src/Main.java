426419cbff6b76c4ca1fc88be1f30c37e0bf701e
hasanashik <hasanashik100@gmail.com>
2022-06-08 17:04:49 +0600

    Java practice float and double variable.

---
+++ float_double/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        float myFloatMinValue = Float.MIN_VALUE;
+        float myFloatMaxValue = Float.MAX_VALUE;
+        System.out.println("myFloatMinValue = "+ myFloatMinValue + " And myFloatMaxValue = "+ myFloatMaxValue);
+
+        double myDoubleMinValue = Double.MIN_VALUE;
+        double myDoubleMaxValue = Double.MAX_VALUE;
+        System.out.println("myDoubleMinValue = "+ myDoubleMinValue + " And myDoubleMaxValue = "+ myDoubleMaxValue);
+
+        int myIntNumber = 5;
+        float myFloatNumber = 5.25f;
+        float myFloatNumber2 = (float) 5.25;
+        double myDoubleNumber = 5d;
+        double myDoubleNumber2 = 5.25;
+
+
+    }
+}
