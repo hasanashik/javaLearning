210edf09a2bbe301af74476d03904378d99bea4c
hasanashik <hasanashik100@gmail.com>
2022-06-08 18:20:39 +0600

    practiced char and boolean type.

---
+++ char_boolean/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println("Only char = "+ myChar + " Using unicode = " + myUnicodeChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+        System.out.println(myFalseBooleanValue);
+    }
+}
