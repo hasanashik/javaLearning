08ac3519d454a362c1a527202dfbe3f994241d08
hasanashik <hasanashik100@gmail.com>
2022-06-08 18:01:22 +0600

    Practice pound to kg conversion using float and double.

---
+++ pound_to_kg/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double one_pound_to_kg = 0.45359237d;
+        float given_pound = 5.9f;
+        double result = given_pound * one_pound_to_kg;
+        System.out.println(given_pound + " Pound = "+ result + " Kg.");
+    }
+}
